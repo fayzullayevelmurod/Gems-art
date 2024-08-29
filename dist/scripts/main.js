458231cdc7f284d2625eee12b8fe5d3e86ee7740
SardorQobilov <sardorqobilov555@gmail.com>
2024-08-29 07:34:44 +0500

    change slider

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,3 +1,13 @@
+// var swiper = new Swiper(".mySwiper", {
+// 	spaceBetween: 8,
+// 	slidesPerView: 5,
+// 	navigation: {
+// 		nextEl: '.swiper-next',
+// 		prevEl: '.swiper-prev',
+// 	},
+// });
+
+
 var swiper = new Swiper(".mySwiper", {
 	spaceBetween: 8,
 	slidesPerView: 5,
@@ -6,4 +16,14 @@ var swiper = new Swiper(".mySwiper", {
 		prevEl: '.swiper-prev',
 	},
 });
-
+var swiper2 = new Swiper(".mySwiper2", {
+	spaceBetween: 8,
+	thumbs: {
+		swiper: swiper,
+	},
+	effect: 'fade',
+	navigation: {
+		nextEl: '.swiper-next',
+		prevEl: '.swiper-prev',
+	},
+});
